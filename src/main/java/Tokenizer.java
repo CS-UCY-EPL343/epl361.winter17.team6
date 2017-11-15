2602ef8aa6becc1b603241ea7595cf41405a4485
stefanos <bluemindset@outlook.com>
2017-11-15 04:52:04 -0500

    New class Tokenizer

---
+++ src/main/java/Tokenizer.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+/**
+ * Created by stephos 15/11/2017.
+ */
+
+
+public class Tokenizer {
+
+private String delimeter;
+
+    Tokenizer(String delimeter){};
+    List <String>[] Tokenize(String s) {return null};
+
+
+
+
+
+}
