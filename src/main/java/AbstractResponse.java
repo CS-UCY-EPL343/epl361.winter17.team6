3eae52dc36667b7dd7815049338a8283e10ce199
stefanos <bluemindset@outlook.com>
2017-11-15 05:10:39 -0500

    New class Abstract Response

---
+++ src/main/java/AbstractResponse.java
@@ -0,0 +1,15 @@
+
+import java.*;
+
+//Created by steph
+
+abstract public class AbstractResponse {
+
+    abstract public String getResponse();
+
+    abstract public void setServiceCaller (ServiceCaller sc);
+
+    abstract public ServiceCaller getServiceCaller();
+
+
+}
