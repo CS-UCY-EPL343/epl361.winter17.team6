73332b6ff83cf8a90703b288807e3271b57af62b
tomis <chadjiminas.chrysostomos@gmail.com>
2017-11-15 11:42:57 +0200

    Created KeyMapper and Tokenizer skeleton.

--- src/main/java/TextParser.java
+++ src/main/java/TextParser.java
@@ -1,17 +1,19 @@
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * Created by tomis on 15/11/2017.
  */
 public class TextParser {
     private String msg;
-
-    TextParser(String msg) {
+    private String DEFAULT_DELIMITER = " ";
+    public TextParser(String msg) {
         this.msg = msg;
     }
 
-    List<String>[] getKeyWords(String msg) {
+    List<String>[] getKeyWords() {
         this.msg = msg;
+        StringTokenizer stk = new StringTokenizer(msg, DEFAULT_DELIMITER);
         return null;
     }
 }
