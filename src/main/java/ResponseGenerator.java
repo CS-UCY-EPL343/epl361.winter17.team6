cca9e6e8dd1754117d92c86748edea3dec5b40f3
stefanos <bluemindset@outlook.com>
2017-11-15 05:11:16 -0500

    New class Response Generator

---
+++ src/main/java/ResponseGenerator.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by steph on 15/11/2017.
+ */
+
+
+public class ResponseGenerator {
+
+    List <String> keyWords;
+    int msgCode ;
+    ServiceCaller sc;
+
+
+    private AbstractResponse mapKeywordsToResponse(){
+
+        return null;
+    }
+
+    private void setMsgCode(int MsgCode){
+
+    }
+
+    public void getMsgCode(){
+
+
+    }
+
+    public String getResponce (List<String> keyWords,int expectedMsgCode,ServiceCaller sc){
+
+        return null;
+    }
+
+
+}
