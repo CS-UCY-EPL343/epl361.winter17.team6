6d1fc90731c27a7a440e5c08b8db873b2e529c54
tomis <chadjiminas.chrysostomos@gmail.com>
2017-11-15 11:38:06 +0200

    added TextParser class.

---
+++ src/main/java/TextParser.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * Created by tomis on 15/11/2017.
+ */
+public class TextParser {
+    private String msg;
+
+    TextParser(String msg) {
+        this.msg = msg;
+    }
+
+    List<String>[] getKeyWords(String msg) {
+        this.msg = msg;
+        return null;
+    }
+}
