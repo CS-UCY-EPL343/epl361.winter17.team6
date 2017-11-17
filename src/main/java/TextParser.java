9a1ba35857df90664441f93d9fd31bb306d754e9
kyriacosar <kyriacosar@gmail.com>
2017-11-17 17:18:33 +0200

    Added textParser Class

---
+++ src/main/java/TextParser.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * Created by Kyriacos on 17/11/2017.
+ */
+public class TextParser {
+    private String msg;
+
+    TextParser(String msg) {
+        this.msg = msg;
+    }
+
+    List<String>[] getKeyWords( ) {
+
+        return null;
+    }
+}
