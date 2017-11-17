2a73bca4a58b688338c8422b3d1fab86357483bd
Stefanos Ioannou <bluemindset@outlook.com>
2017-11-17 23:19:30 +0200

    response

--- src/main/java/ListOfRestaurantNamesResponse.java
+++ src/main/java/ListOfRestaurantNamesResponse.java
@@ -2,9 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListOfRestaurantNamesResponse extends AbstractResponse {
+    private ServiceCaller sc;
 
     public String getResponse(){
+
         List<Restaurant> restaurantList = new ArrayList<Restaurant>();
+        sc = getServiceCaller();
         restaurantList = sc.getMatchingRestaurants();
 
         String response = restaurantList.toString();
