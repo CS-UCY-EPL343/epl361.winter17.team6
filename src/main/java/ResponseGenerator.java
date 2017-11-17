ad5231f445fa7ff833ed8df884522125f03c5ce1
Stefanos Ioannou <bluemindset@outlook.com>
2017-11-17 23:10:00 +0200

    Build ResponseGenerator

--- src/main/java/ResponseGenerator.java
+++ src/main/java/ResponseGenerator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,14 +12,27 @@ public class ResponseGenerator {
     List <String> keyWords;
     int msgCode ;
     ServiceCaller sc;
+    public static final int LIST_RESTAURANT = 1;
 
 
     private AbstractResponse mapKeywordsToResponse(){
+        ArrayList<String> KeyWords = new ArrayList<>();
+        AbstractResponse ab_response = new AbstractResponse() {
+            @Override
+            public String getResponse() {
 
-        return null;
+
+                return null;
+            }
+        };
+
+        return ab_response;
     }
 
     private void setMsgCode(int MsgCode){
+
+
+            this.msgCode = MsgCode;
 
     }
 
@@ -29,7 +43,25 @@ public class ResponseGenerator {
 
     public String getResponce (List<String> keyWords,int expectedMsgCode,ServiceCaller sc){
 
-        return null;
+        String response_message= "No response";
+
+        AbstractResponse abstract_response_class;
+
+        switch (expectedMsgCode) {
+            case 1:
+                abstract_response_class = new ListOfRestaurantNamesResponse();
+                abstract_response_class.setServiceCaller(sc);
+
+                response_message=abstract_response_class.getResponse();
+
+                break;
+
+
+        }
+
+
+
+        return response_message ;
     }
 
 
