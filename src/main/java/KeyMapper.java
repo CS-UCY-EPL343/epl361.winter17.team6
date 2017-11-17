5f90a3b46b27b4fccf0fe8af51186c38ffb5b1f0
kyriacosar <kyriacosar@gmail.com>
2017-11-17 17:18:55 +0200

    Added KeyMapper Class

---
+++ src/main/java/KeyMapper.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Kyriacos on 17/11/2017.
+ */
+public class KeyMapper {
+    private HashMap<String, String> internetSlangToWordMap;
+
+    public List<String> getMainKeyWords(List <String> words){
+
+        return null;
+    }
+
+}
