854939679e9cccc24984e0e15bf17bbe715254dd
tomis <chadjiminas.chrysostomos@gmail.com>
2017-11-17 22:32:38 +0200

    Create TestApp to test.

---
+++ src/main/java/TestApp.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * Created by tomis on 17/11/2017.
+ */
+public class TestApp {
+    public String getChatbotResponse(String usrMsg) {
+        TextParser tp = new TextParser(usrMsg);
+        List<String> parsedUserMessage = tp.getKeyWords();
+
+
+    }
+    public static void main(String args[]) {
+
+
+    }
+}
