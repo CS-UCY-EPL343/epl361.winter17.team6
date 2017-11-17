cc6b756245f4896435cc37bb699608ee3cc39364
kyriacosar <kyriacosar@gmail.com>
2017-11-17 17:42:33 +0200

    Add Tokenizer Class methods and attributes.

--- src/main/java/Tokenizer.java
+++ src/main/java/Tokenizer.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tokenizer {
+
+
+    private String delimiter;
+
+    public List<String> tokenize(String s) {
+        List<String> tokens = new ArrayList<>();
+
+        return tokens;
+    }
+
+    public void setDelimiter(String s) {
+        this.delimiter = s;
+    }
+
 }
