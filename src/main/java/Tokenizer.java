17a363d59fc45375b59c0128bff76e1f7e24a505
kyriacosar <kyriacosar@gmail.com>
2017-11-17 17:54:48 +0200

    Added tokenize method.

--- src/main/java/Tokenizer.java
+++ src/main/java/Tokenizer.java
@@ -1,13 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Tokenizer {
 
 
-    private String delimiter;
+    private String delimiter = " ";
 
     public List<String> tokenize(String s) {
         List<String> tokens = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(s);
+        while (st.hasMoreTokens()) {
+            tokens.add(st.nextToken());
+        }
 
         return tokens;
     }
