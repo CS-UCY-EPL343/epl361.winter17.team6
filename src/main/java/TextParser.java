94f714d41edcd91017e969839b9b3dde1554fa6d
kyriacosar <kyriacosar@gmail.com>
2017-11-17 22:45:09 +0200

    Updated getKeyWords method of Class TextParser.

--- src/main/java/TextParser.java
+++ src/main/java/TextParser.java
@@ -4,14 +4,17 @@ import java.util.List;
  * Created by Kyriacos on 17/11/2017.
  */
 public class TextParser {
+
     private String msg;
 
-    TextParser(String msg) {
+    public TextParser(String msg) {
         this.msg = msg;
     }
 
-    List<String> getKeyWords( ) {
-
+    public List<String> getKeyWords(){
+        Tokenizer tk = new Tokenizer(this.msg);
+        List<String> tokens = tk.tokenize();
+        KeyMapper.getMainKeyWords(tokens);
         return null;
     }
 }
