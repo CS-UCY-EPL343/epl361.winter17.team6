2666cce961dbec47b63bebbe960d194b69a1af01
Stefanos Ioannou <bluemindset@outlook.com>
2017-11-18 13:17:17 +0200

    Changes in Printing restaurants

--- src/main/java/ListOfRestaurantNamesResponse.java
+++ src/main/java/ListOfRestaurantNamesResponse.java
@@ -5,15 +5,18 @@ public class ListOfRestaurantNamesResponse extends AbstractResponse {
 
 
     public String getResponse(){
-
+        StringBuilder response = new StringBuilder();
         List<Restaurant> restaurantList = new ArrayList<Restaurant>();
 
         restaurantList =  getServiceCaller().getMatchingRestaurants();
 
-        String response = restaurantList.toString();
+        response.append("The restaurants that you have chosen are:" + "\n");
+        for (Restaurant restaurant_name: restaurantList)
+              response.append(restaurant_name.getName()+ "\n");
 
 
-        return response;
+
+        return response.toString();
     }
 
 }
