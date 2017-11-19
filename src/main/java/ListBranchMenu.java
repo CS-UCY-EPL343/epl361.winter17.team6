3e45c21a64a7dc6621815189e4156df23c4434f9
stefanos <bluemindset@outlook.com>
2017-11-19 12:19:23 -0500

    Printing

--- src/main/java/ListBranchMenu.java
+++ src/main/java/ListBranchMenu.java
@@ -1,8 +1,9 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListBranchMenu extends AbstractResponse{
-
+    private final static String MENU_ITEM_CLASS_NAME = "mi-class";
     private int restaurantId;
 
     public void setRestaurantId(int resId){
@@ -12,11 +13,18 @@ public class ListBranchMenu extends AbstractResponse{
     public String getResponse() {
 
         StringBuilder response = new StringBuilder();
-
-
         getServiceCaller().setSelectedRestaurant(restaurantId);
+        BranchMenu branchMenu = getServiceCaller().getSelectedRestaurantMenu();
+        List<FoodCategory> branchMenuFoodCategories = branchMenu.getFoodCategories();
+//        for(FoodCategory fc : branchMenuFoodCategories){
+//            response.append("<ul>" + fc.getName() + "</ul>");
+//            List<MenuItem> fcMenuItems = fc.getMenuItems();
+//            for(MenuItem mi : fcMenuItems) {
+//                response.append("<li class=\"\">" + mi.getName() + "</li>");
+//            }
+//
+//        }
 
-        response.append("Τα εστιατορία για τις επιλογες σας είναι :" + "\n");
 
         return response.toString();
     }
