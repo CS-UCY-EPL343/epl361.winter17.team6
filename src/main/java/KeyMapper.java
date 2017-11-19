84a1bd236ba006871fe30b1ab4d234690d352480
kyriacosar <kyriacosar@gmail.com>
2017-11-19 14:59:05 +0200

    Updated getKeyWords method of TextParser Class

--- src/main/java/KeyMapper.java
+++ src/main/java/KeyMapper.java
@@ -6,11 +6,13 @@ import java.util.List;
  * Created by Kyriacos on 17/11/2017.
  */
 public class KeyMapper {
+
     private HashMap<String, String> internetSlangToWordMap;
 
     public static List<String> getMainKeyWords(List<String> words){
         List<String> mainKeyWords = new ArrayList<>();
-        return mainKeyWords;
+
+        return words;
     }
 
 }
