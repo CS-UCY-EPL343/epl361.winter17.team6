25046de5a7cf496ceae62e294c56ad22b2ba35e1
nikogior <nikogior@hotmail.com>
2017-11-22 22:42:48 +0200

    changed history

--- ChatbotUI/Public/js/ChatbotUIcode.js
+++ ChatbotUI/Public/js/ChatbotUIcode.js
@@ -64,8 +64,13 @@ $(document).ready(function () {
                         $('#chatborder').scrollTop($('#chatborder')[0].scrollHeight);
                     }
                     else {
-                        $('#chatborder').append('<ul class="bubble2" >' + msgsReceived[i]['content'] + '</ul>');
-                        $('#chatborder').scrollTop($('#chatborder')[0].scrollHeight);
+
+
+                        // var msgres = "<b>" + botName + ":</b> " +
+                        //     "<span id='chattimestamp'>" + days[d1.getDay()] + " at " + d1.getHours() + ":" + d1.getMinutes() + ":" + d1.getSeconds() + "</span>" +
+                        //     "<p>" + msgsReceived[i]['content'] + "</p>";
+                        // $('#chatborder').append('<ul class="bubble2" >' + msgres + '</ul>');
+                        // $('#chatborder').scrollTop($('#chatborder')[0].scrollHeight);
                     }
                 }
             }
